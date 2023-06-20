7a45ec252e95c63fc00e82d5545a887616f45bad
Odafe Aror <odafe41@gmail.com>
2023-06-20 07:35:53 +0000

    --

--- src/components/navigation/MobileSidebar.tsx
+++ src/components/navigation/MobileSidebar.tsx
@@ -56,14 +56,15 @@ export const MobileSidebar = () => {
         >
           <Flex alignItems="center">
             <Avatar
-              name="John Wick"
+              name="Hannah Waddingham"
+              src="https://plus.unsplash.com/premium_photo-1663054688278-ebf09d654d33?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1287&q=80"
               bgColor="brand.primary.kappa"
               color="black.100"
               size="sm"
               mr={2}
             />
             <Box>
-              <Text textStyle="base">John Wick</Text>
+              <Text textStyle="base">Hannah Waddingham</Text>
             </Box>
           </Flex>
           {/* <IconButton
